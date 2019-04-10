5cdbfdf49110ce0c8a5a3ec4c9e494056f25b123
Kinga Jackowska <jackowska.kinga@gmail.com>
2019-04-10 23:11:56 +0200

    commit message

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+
+    // Disable dragging
+    freeScroll: true,
+    
+    // disable previous & next buttons and dots
+    prevNextButtons: false,
+    pageDots: false
+  });
