b9f7ca4b4f78f3e8770a47a4e9791f9a146333f7
Kinga Jackowska <jackowska.kinga@gmail.com>
2019-04-11 01:17:08 +0200

    commit message

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-$('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true,
+  var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+});
 
-    // Disable dragging
-    freeScroll: true,
-    
-    // disable previous & next buttons and dots
-    prevNextButtons: false,
-    pageDots: false
-  });
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
+
+// vanilla JS
+var flkty = new Flickity( '.carousel', {
+    hash: true,
+});
